4692504f5dd490f7cf2086488cc9fbc9df2e067f
ebaysamsam3865 <87864472+ebaysamsam3865@users.noreply.github.com>
2021-07-27 13:49:46 -0500

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -4,3 +4,11 @@ provider "aws" {
   secret_key=var.AWS_SECRET_KEY 
 }
 
+resource "aws_instance" "web" {
+  ami           = "ami-0dc2d3e4c0f9ebd18"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "SudhakarWorld"
+  }
+}
