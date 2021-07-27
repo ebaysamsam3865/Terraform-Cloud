9c08a8627b1df9acc5a0d8ef92ceb44e580f027a
ebaysamsam3865 <87864472+ebaysamsam3865@users.noreply.github.com>
2021-07-27 11:28:51 -0500

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 resource "aws_instance" "web" {
   ami           = "ami-0dc2d3e4c0f9ebd18"
-  instance_type = "t.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "MyHelloWorld"
